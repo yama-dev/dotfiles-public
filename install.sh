7f51446f8b3a962ce808e9f2d7e4de7e0aa428a5
yama-dev <tatsuya.yamamoto69@gmail.com>
2021-07-30 10:29:06 +0900

    add install.sh apps

--- install.sh
+++ install.sh
@@ -2,3 +2,25 @@
 # Mac Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+brew install --cask appcleaner
+brew install --cask clipy
+brew install --cask google-japanese-ime
+brew install --cask karabiner-elements
+brew install --cask the-unarchiver
+brew install --cask firefox
+brew install --cask google-chrome
+brew install --cask slack
+
+brew install --cask postman
+brew install --cask sequel-pro
+brew install --cask sourcetree
+brew install --cask openvpn-connect
+brew install --cask chrome-remote-desktop-host
+
+brew install --cask macvim
+brew install --cask visual-studio-code
+
+brew install --cask virtualbox
+brew install --cask docker
+brew install --cask iterm2
+
